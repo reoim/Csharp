55a600f49876d0af32c61c357077f6b7f2241d83
Reo Dongmin Lee <reoim10@gmail.com>
2016-08-07 22:36:20 +1000

    Property and Encapsulation

---
+++ Encapsulation/Encapsulation/Person.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Encapsulation
+{
+    public class Person
+    {
+
+        /*
+        private DateTime _birthdate;
+
+        public void SetBirthdate(DateTime birthdate)
+        {
+            _birthdate = birthdate;
+        }
+
+        public DateTime GetBirthdate()
+        {
+            return _birthdate;
+        }
+        */
+
+
+
+        //auto implemented properties
+        public string Name { get; set; }
+        public string Username { get; set; }
+        public DateTime Birthdate { get; private set; }
+        //use private for set. so it becomes readonly. cannot be modified. Create constructor to set birthday.
+
+
+        public Person(DateTime birthdate)
+        {
+            Birthdate = birthdate;
+        }
+
+        
+        public int Age
+        {
+            get
+            {
+                var timeSpan = DateTime.Today - Birthdate;
+                var years = timeSpan.Days / 365;
+
+                return years;
+            }
+        }
+    }
+}
---
+++ Encapsulation/Encapsulation/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Encapsulation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var person = new Person(new DateTime(1986, 2, 2));
+
+            //person.SetBirthdate(new DateTime(1986, 2, 2));
+            //Console.WriteLine(person.GetBirthdate()); 
+
+            //person.Birthdate = new DateTime(1986, 2, 2);
+            Console.WriteLine(person.Age);
+        }
+    }
+}
