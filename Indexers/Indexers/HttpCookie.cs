b28e3d9d653a2d16add832e803cc89f878e88852
Reo Dongmin Lee <reoim10@gmail.com>
2016-08-08 23:42:16 +1000

    Indexers

---
+++ Indexers/Indexers/HttpCookie.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Indexers
+{
+    class HttpCookie
+    {
+        private Dictionary<string, string> _dictionary = new Dictionary<string, string>();
+
+        public string Name { get; set; }
+
+        public string this[string key]
+        {
+            get { return _dictionary[key]; }
+            set { _dictionary[key] = value; }
+        }
+
+    }
+}
---
+++ Indexers/Indexers/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Indexers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var cookies = new HttpCookie();
+            cookies["name"] = "Reo";
+            Console.WriteLine(cookies["name"]);
+
+        }
+    }
+}
