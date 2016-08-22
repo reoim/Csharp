2b15ed613b711979cbf9783e0df50f896fa2122d
Reo Dongmin Lee <reoim10@gmail.com>
2016-08-22 19:49:52 +1000

    Polymorphism

---
+++ AbstractClasses/AbstractClasses/Shape.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractClasses
+{
+    public abstract class Shape
+    {
+        public int Width { get; set; }
+        public int Height { get; set; }
+
+        public abstract void Draw();
+
+        public void Copy()
+        {
+            Console.WriteLine("Copy shae into clipboard.");
+        }
+    }
+}
