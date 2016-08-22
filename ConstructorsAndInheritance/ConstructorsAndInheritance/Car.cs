bb937f84baf23328059fe8c897f560364ad92af9
Reo Dongmin Lee <reoim10@gmail.com>
2016-08-22 12:51:12 +1000

    Constructors and Inheritance

---
+++ ConstructorsAndInheritance/ConstructorsAndInheritance/Car.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstructorsAndInheritance
+{
+    public class Car : Vehicle
+    {
+
+        public Car(string registrationNumber)
+            : base(registrationNumber)
+        {
+            Console.WriteLine("Car is being initialized. {0}", registrationNumber);
+        }
+
+
+
+    }
+}
---
+++ ConstructorsAndInheritance/ConstructorsAndInheritance/Vehicle.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstructorsAndInheritance
+{
+    public class Vehicle
+    {
+        private readonly string _registrationNumber;
+
+        public Vehicle(string registrationNumber)
+        {
+            _registrationNumber = registrationNumber;
+
+            Console.WriteLine("Vehicle is being initialized. {0}", registrationNumber);
+
+        }
+
+    }
+}
