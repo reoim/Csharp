00301e54497a398ea983b434cd71fd7bca526e32
Reo Dongmin Lee <reoim10@gmail.com>
2016-08-22 17:52:25 +1000

    Method overriding

---
+++ MethodOverriding/MethodOverriding/Shape.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodOverriding
+{
+    public class Circle : Shape
+    {
+        public override void Draw()
+        {
+            Console.WriteLine("Draw a circle");
+        }
+    }
+
+    public class Rectangle : Shape
+    {
+        public override void Draw()
+        {
+            Console.WriteLine("Draw a rectangle");
+        }
+
+
+    }
+
+    public class Shape
+    {
+        public int Width { get; set; }
+        public int Height { get; set; }
+       
+       
+        public virtual void Draw()
+        {
+
+        }
+
+    }
+}
