1822a34931507b3745a834da71dab99e7f0163c5
Reo Dongmin Lee <reoim10@gmail.com>
2016-08-26 22:54:00 +1000

    Lamda Experession

---
+++ LamdaExpressions/LamdaExpressions/BookRepository.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LamdaExpressions
+{
+    public class BookRepository
+    {
+        public List<Book> GetBooks()
+        {
+            return new List<Book>
+            {
+                new Book() {Title="Title 1", Price = 4 },
+                new Book() {Title="Title 2", Price = 9 },
+                new Book() {Title="Title 3", Price = 15 }
+
+            };
+        }
+
+    }
+}
---
+++ LamdaExpressions/LamdaExpressions/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LamdaExpressions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var books = new BookRepository().GetBooks();
+
+            //var cheapBooks = books.FindAll(IsCheaperThan10Dollars);
+
+            var cheapBooks = books.FindAll(b => b.Price < 10);
+
+            foreach(var book in cheapBooks)
+            {
+                Console.WriteLine(book.Title);
+            }
+        }
+
+        static bool IsCheaperThan10Dollars(Book book)
+        {
+            return book.Price < 10;
+        }
+    }
+}
